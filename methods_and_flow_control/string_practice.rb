a7afcac17c3d8a829ce3f1e122d2e80d9eeae0fc
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-08 20:48:32 -0600

    Modified and updated string_practice.rb
    *Implemented the feedback
    
    [#131780281]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/env ruby
 
 string_practice = "this is a string to practice with"
-practice = "practice "
-the_string = "The string '"
-length_of_characters = "' has 33 characters"
+
 puts string_practice
 puts string_practice.capitalize
 puts string_practice.upcase
 puts string_practice.gsub("string", "'string'").capitalize
-puts the_string + string_practice + length_of_characters
+puts "The string '" + string_practice + "' has " + string_practice.length.to_s + " characters"
 puts string_practice.reverse
-puts practice * 3
+puts string_practice[-13..-5] * 3 + "!"
