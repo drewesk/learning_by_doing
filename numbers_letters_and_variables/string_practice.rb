12eb1b81e798b049f6d915558678f9c4bd396257
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-09 20:53:21 -0600

    rebase of updated files

---
+++ numbers_letters_and_variables/string_practice.rb
@@ -0,0 +1,11 @@
++#!/usr/bin/env ruby
++
++string_practice = "this is a string to practice with"
++
++puts string_practice
++puts string_practice.capitalize
++puts string_practice.upcase
++puts string_practice.gsub("string", "'string'").capitalize
++puts "The string '" + string_practice + "' has " + string_practice.length.to_s + " characters"
++puts string_practice.reverse
++puts string_practice[-13..-5] * 3 + "!"
