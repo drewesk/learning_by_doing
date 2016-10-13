0014a70134dccc7083c6289100e7f63f5dd10877
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-13 13:59:04 -0600

    Fixed bug and replaced interpolation with peram var
    * Fixed a typo on line 6
    
    * replaced interpolation with perameter variable on line 12
    
    [#131780283]

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -3,13 +3,13 @@
 numbers = (1..100).to_a
 numbers.each do |number|
   if (number % 3).zero? && (number % 5).zero?
-    puts'FizzBuzz'
+    puts 'FizzBuzz'
   elsif (number % 5).zero?
     puts 'Buzz'
   elsif (number % 3).zero?
     puts 'Fizz'
   else
-    puts "#{number}"
+    puts number
   end
 end
 
